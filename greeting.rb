b75b36206968492cce9943431bb29e9d4511b183
William Gao <gaowhui@gmail.com>
2017-11-02 20:28:56 +0000

    cp6 done

--- greeting.rb
+++
@@ -1,9 +0,0 @@
-def welcome
-  result = ARGV[1..-1]  
-    result.each do |y|
-        p "#{ARGV[0]} #{y}"
-    end
-
-end
-
-welcome
--- hello_world.rb
+++
@@ -1,8 +0,0 @@
-def hello_world
-    ARGV.each do |x|
-        p "hello, #{arg}"
-    end
-end
-
-
-hello_world
